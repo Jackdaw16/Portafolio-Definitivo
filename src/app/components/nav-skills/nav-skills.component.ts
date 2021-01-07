fdf218a5b11d52d78a4b453c197cf457ce451338
Jackdaw16 <alejandro.demetrio13@gmail.com>
2021-01-07 14:53:08 +0000

    rethinking and others responsives changes

--- src/app/components/nav-skills/nav-skills.component.ts
+++ src/app/components/nav-skills/nav-skills.component.ts
@@ -9,12 +9,12 @@ export class NavSkillsComponent implements OnInit {
 
   /* Class */
   /*1366 x 768*/
-  notActive = 'bg-secondary text-primary font-quick hover:bg-primary hover:text-secondary text-xs p-1 md:text-base md:p-2 lg:text-xl lg:px-3 lg:py-2.5 xl:text-sm xl:p-1.5';
-  active = 'bg-primary text-secondary font-quick text-xs p-1 md:text-base md:p-2 lg:text-xl lg:px-3 lg:py-2.5 xl:text-sm xl:p-1.5';
-  notActiveRoundedRight = 'bg-secondary text-primary font-quick hover:bg-primary hover:text-secondary text-xs p-1 md:text-base md:p-2 lg:text-xl lg:px-3 lg:py-2.5 xl:text-sm xl:p-1.5 rounded-tr-full rounded-br-full';
-  activeRoundedRight = 'bg-primary text-secondary font-quick text-xs p-1 md:text-base md:p-2 lg:text-xl lg:px-3 lg:py-2.5 xl:text-sm xl:p-1.5 rounded-tr-full rounded-br-full';
-  notActiveRoundedLeft = 'bg-secondary text-primary font-quick hover:bg-primary hover:text-secondary text-xs p-1 md:text-base md:p-2 lg:text-xl lg:px-3 lg:py-2.5 xl:p-1.5 xl:text-sm rounded-tl-full rounded-bl-full';
-  activeRoundedLeft = 'bg-primary text-secondary font-quick hover:bg-primary hover:text-secondary text-xs p-1 md:text-base md:p-2 lg:text-xl lg:px-3 lg:py-2.5 xl:p-1.5 xl:text-sm rounded-tl-full rounded-bl-full';
+  notActive = 'bg-secondary text-primary font-quick hover:bg-primary hover:text-secondary text-xs p-1 md:text-base md:p-2 lg:text-2xl lg:px-3 lg:py-2.5 xl:text-sm xl:p-1.5';
+  active = 'bg-primary text-secondary font-quick text-xs p-1 md:text-base md:p-2 lg:text-2xl lg:px-3 lg:py-2.5 xl:text-sm xl:p-1.5';
+  notActiveRoundedRight = 'bg-secondary text-primary font-quick hover:bg-primary hover:text-secondary text-xs p-1 md:text-base md:p-2 lg:text-2xl lg:px-3 lg:py-2.5 xl:text-sm xl:p-1.5 rounded-tr-full rounded-br-full';
+  activeRoundedRight = 'bg-primary text-secondary font-quick text-xs p-1 md:text-base md:p-2 lg:text-2xl lg:px-3 lg:py-2.5 xl:text-sm xl:p-1.5 rounded-tr-full rounded-br-full';
+  notActiveRoundedLeft = 'bg-secondary text-primary font-quick hover:bg-primary hover:text-secondary text-xs p-1 md:text-base md:p-2 lg:text-2xl lg:px-3 lg:py-2.5 xl:p-1.5 xl:text-sm rounded-tl-full rounded-bl-full';
+  activeRoundedLeft = 'bg-primary text-secondary font-quick hover:bg-primary hover:text-secondary text-xs p-1 md:text-base md:p-2 lg:text-2xl lg:px-3 lg:py-2.5 xl:p-1.5 xl:text-sm rounded-tl-full rounded-bl-full';
 
   constructor() { }
 
--- src/app/components/nav-skills/web-skills/web-skills.component.ts
+++ src/app/components/nav-skills/web-skills/web-skills.component.ts
@@ -7,7 +7,7 @@ import anime from 'animejs/lib/anime.es.js';
 })
 export class WebSkillsComponent implements OnInit {
 
-  defaultClass = 'block w-14 h-14 md:w-20 md:h-20 xl:w-16 xl:h-16 p-2';
+  defaultClass = 'block w-14 h-14 md:w-20 md:h-20 lg:w-28 lg:h-28 xl:w-16 xl:h-16 p-2';
 
   constructor() { }
 
