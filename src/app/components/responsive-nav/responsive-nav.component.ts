2a25444ca2bc57b392eb5bd6999360730db01ecc
Jackdaw16 <alejandro.demetrio13@gmail.com>
2021-02-10 19:26:45 +0000

    theme switch and responsive navbar fixed

--- src/app/components/responsive-nav/responsive-nav.component.ts
+++ src/app/components/responsive-nav/responsive-nav.component.ts
@@ -9,11 +9,11 @@ export class ResponsiveNavComponent implements OnInit {
   hidden = 'hidden';
   notHidden = 'flex';
 
+  hiddeAnim = 'transform translate-x-full';
+  showAnim = 'transform translate-x-0.4';
+
   theme = 'white';
   html = document.getElementsByTagName('html')[0];
-
-  darkTheme = 'transform translate-x-0.4';
-  lightTheme = 'transform -translate-x-6';
 
   constructor() { }
 
@@ -24,23 +24,6 @@ export class ResponsiveNavComponent implements OnInit {
       this.html.classList.add(this.theme);
     } else {
       return;
-    }
-  }
-
-  toggle(): void {
-    if (this.theme === 'dark') {
-      this.theme = 'white';
-      this.html.classList.remove('dark');
-      this.html.classList.add(this.theme);
-
-      localStorage.setItem('theme', this.theme);
-
-    } else if (this.theme === 'white') {
-      this.theme = 'dark';
-      this.html.classList.remove('white');
-      this.html.classList.add(this.theme);
-
-      localStorage.setItem('theme', this.theme);
     }
   }
 
